4f221dba966ca6d776aa630131e0af49fc1e4ff6
Sanko Robinson <sanko@cpan.org>
2026-02-21 17:57:16 -0500

    Working my way through rewriting examples in new API

--- eg/thread_pool.pl
+++ eg/thread_pool.pl
@@ -1,33 +1,34 @@
 use v5.40;
 use blib;
-use Acme::Parataxis;
-use Time::HiRes qw[time];
+use Acme::Parataxis qw[:all];
+use Time::HiRes     qw[time];
 $|++;
-Acme::Parataxis::run(
-    sub {
-        say 'Main: Thread pool size is ' . Acme::Parataxis::get_thread_pool_size();
-        my @futures;
-        for my $id ( 1 .. 3 ) {
-            push @futures, Acme::Parataxis->spawn(
-                sub {
-                    my $sleep_time = int( rand(2000) ) + 500;
-                    say "  [Worker $id] FID: " . Acme::Parataxis->fid . " will sleep for ${sleep_time}ms in background";
-                    my $start = time();
 
-                    # This yields to main, background thread sleeps, scheduler resumes us
-                    Acme::Parataxis->await_sleep($sleep_time);
-                    my $elapsed = int( ( time() - $start ) * 1000 );
-                    say "  [Worker $id] Woke up after ${elapsed}ms!";
-                    return "Done $id";
-                }
-            );
-        }
-        say 'Main: All workers spawned. Waiting for completion...';
-        my $start_time = time();
+# Demonstrate dynamic thread pool growth and modern API
+async {
+    say 'Main: Initial thread pool size is ' . Acme::Parataxis::get_thread_pool_size();
+    my @futures;
+    for my $id ( 1 .. 5 ) {
+        push @futures, fiber {
+            my $fid        = current_fid();
+            my $sleep_time = int( rand(1000) ) + 500;
+            say "  [Worker $id] Fiber #$fid will sleep for ${sleep_time}ms in background pool";
+            my $start = time();
 
-        # Wait for all workers to finish via futures
-        $_->await for @futures;
-        say 'Main: All workers finished!';
-        printf 'Total wallclock time: %.2fs (Note how they ran in parallel!)', time() - $start_time;
+            # This yields to main, background thread sleeps, scheduler resumes us
+            await_sleep($sleep_time);
+            my $elapsed = int( ( time() - $start ) * 1000 );
+            say "  [Worker $id] Fiber #$fid woke up after ${elapsed}ms!";
+            return "Done $id";
+        };
     }
-);
+    say 'Main: All workers spawned. Waiting for completion...';
+    say 'Main: Thread pool size during load: ' . Acme::Parataxis::get_thread_pool_size();
+    my $start_time = time();
+
+    # Wait for all workers to finish via futures using modern 'await'
+    await $_ for @futures;
+    say 'Main: All workers finished!';
+    printf "Total wallclock time: %.2fs\n", time() - $start_time;
+    say 'Main: Final thread pool size: ' . Acme::Parataxis::get_thread_pool_size();
+};
