8b1ecd974869539692d8f5fafe7aa4a3d3a98b19
Sanko Robinson <30663+sanko@users.noreply.github.com>
2026-09-11 21:53:26 -0400

    Separate your fd_set variables for read and write again

--- lib/Acme/Parataxis/Channel.pm
+++ lib/Acme/Parataxis/Channel.pm
@@ -5,16 +5,16 @@ use Acme::Parataxis::Semaphore;
 class Acme::Parataxis::Channel v0.1.0 {
     field $capacity : param //= 2_000_000_000;
     field $sem_get = Acme::Parataxis::Semaphore->new( count => 0 );
-    field $sem_put = Acme::Parataxis::Semaphore->new( count => $capacity - 1 );
+    field $sem_put = Acme::Parataxis::Semaphore->new( count => $capacity );
     field @data : reader;
     ADJUST {
         $capacity >= 1 or die "Channel capacity must be >= 1 (got $capacity)\n";
     }
 
     method put ($value) {
+        $sem_put->down;
         push @data, $value;
         $sem_get->up;
-        $sem_put->down;
         1;
     }
 
